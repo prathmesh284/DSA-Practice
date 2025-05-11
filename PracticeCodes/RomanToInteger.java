41d0662a784c5cccf01174a712e038befc8e098b
Pratham <jadhavprathmesh284@gmail.com>
2025-05-11 11:42:54 +0530

    roman to integer problem

---
+++ PracticeCodes/RomanToInteger.java
@@ -0,0 +1,32 @@
+//Roman to Integer
+//By using HashMap
+
+import java.util.HashMap;
+class Solution{
+    private static int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int result = 0;
+        for( int i=0; i<s.length(); i++){
+            if(i>0 && map.get(s.charAt(i)) > map.get(s.charAt(i-1))){
+                result += (map.get(s.charAt(i)) - 2*map.get(s.charAt(i-1)));
+            }else{
+                result += map.get(s.charAt(i));
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        String s = "IV";
+        System.out.println(""+ romanToInt(s));
+    }
+}
+
+//Time Complexity: O(n)
+//Space Complexity: O(map.size())
