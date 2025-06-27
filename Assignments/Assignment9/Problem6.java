1a15ed27c3377f19ab066c3953920bd45a4d8cfe
Pratham <jadhavprathmesh284@gmail.com>
2025-06-27 19:41:54 +0530

    Assignment 9

--- Assignments/Assignment9/Problem6.java
+++ Assignments/Assignment9/Problem6.java
@@ -1,28 +1,36 @@
 //Next Permutation (Recursive)
 
 class Solution {
-    static String permutation(String str,int ind,int count,String ans,boolean[] visited,String res){
+    static String permutation(String str,int ind,int count,String ans,boolean[] visited){
         for(int i = 0; i < str.length(); i++){
             if(ans.length() == str.length()){
-                if(ans.equals(str) || count < 2) count++; 
+
+                if(ans.equals(str)) {
+                    count = 1; 
+                }else if(count >= 1){
+                    count++;
+                }else{
+                    count = 0;
+                }
+                StringBuilder res = new StringBuilder();
                 if(count == 2){
-                    res = ans;
+                    res = res.append(ans);
                     System.out.println(res);
                     break;
                 }
-                return res;
+                return res.toString();
             } 
             if(visited[i]){
                 continue;
             }
             visited[i] = true;
-            permutation(str,ind+1,count,ans+str.charAt(i),visited,res);
+            permutation(str,ind+1,count,ans+str.charAt(i),visited);
             visited[i] = false;
         }
-        return res;
+        return "";
     }
     public static void main(String[] args){
         String str = "123";
-        System.out.println(permutation(str,0,0,"",new boolean[str.length()],""));
+        permutation(str,0,0,"",new boolean[str.length()]);
     }
 }
